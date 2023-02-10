d5a7c480631a7fdcc93ce34a0522a5376c097b30
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2023-02-11 00:22:51 +0100

    ...

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,82 @@
-export default class Header {
-    constructor(height, width, bgColor) {
-        this.width = width;
-        this.height = height;
-        this.bgColor = bgColor;
 
+/**
+ *
+ * @class
+ * @name Header
+ *
+ * @description Create a header.
+ *
+ * @see {@link setLogo()}
+ * @see {@link setStyle()}
+ *
+ * @example
+ *  const newHeader = new Header();
+ *  newHeader.setLogo("./assests/logo_icon.svg", "Test Inc.");
+ *  newHeader.setStyle("background-color", "aqua");
+ *
+ * @returns New Header to your page.
+ *
+ */
+class Header {
+    /**
+     * 
+     * @returns {object} this
+     * 
+     * @constructor
+     *
+     */
+    constructor() {
         $('body').append('<div class="header"></div>');
-        $('.header').css('width', this.width);
-        $('.header').css('height', this.height);
-        $('.header').css('background-color', this.bgColor);
-    }
-
-    setLogo(logo) {
         
+        return this;
     }
 
+    /**
+     *  
+     * @name setLogo()
+     * @description Setup logo for header.
+     *
+     * @see {@link Header}
+     *
+     * @param {string} logoPath - Enter the path to icon image.
+     * @param {string} logoText -  Enter the name of the company.
+     *
+     * @example 
+     * newHeader.setLogo("./assests/logo_icon.svg", "Test Inc.");
+     *
+    */
+    setLogo(logoPath, logoText) {
+        $('.header').append('<a id="logoLinkBox" href="./index.html"></a>');
+        const logoLinkBox = $('#logoLinkBox');    
+
+        if(logoPath != "") {
+            logoLinkBox.append('<img class="logoIcon" src="'+logoPath+'">');
+        }
+
+        if(logoText != "") {
+            logoLinkBox.append('<span class="logoText">'+logoText+'</span>');
+        }   
+    }
+
+    /**
+     *
+     * @name setStyle()
+     * @description Customize CSS styling of the header.
+     * 
+     * @see {@link Header}
+     * 
+     * @param {STRING} targetAttribute Enter the name of the atrribute you want to customize. 
+     * @param {STRING} targetValue Enter the new value of the attrinute.
+     * 
+     * @example 
+     * newHeader.setStyle("background-color", "aqua");
+     * 
+    */
     setStyle(targetAttribute, targetValue) {
         $('.header').css(targetAttribute, targetValue);
     }
+
+
 }
+
+export default Header;
